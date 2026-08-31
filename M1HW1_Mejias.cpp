bda0a33eeea184223ab454b743af8d029bbc4a9a
Just Andrea <mejiasa3102@student.faytechcc.edu>
2026-08-31 23:53:30 +0000

    Finished M1HW1_Mejias.cpp and ran program successfully

---
+++ M1HW1_Mejias.cpp
@@ -0,0 +1,43 @@
+// Header comments go here 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // Movie name
+    string movieName ="KPop Demon Hunters";
+
+    // Movie's release year
+    int releaseYear = 2025;
+
+    // Movie's box office gross earnings
+    double BOGross = 24.7;
+
+    // Display movie information
+    cout << movieName << endl; 
+    cout << releaseYear << endl; 
+    cout << "$" << BOGross << " million" << endl;
+    cout << "" << endl; 
+
+    // Display movie summary
+    cout << "Movie Summary" << endl;
+    cout << "" << endl; 
+    cout << "A K-pop trio of female singers go on world tours while secretly battling demons that threaten" << endl;
+    cout << "to destroy a barrier against the demons." << endl;
+    cout << "When a rival boy band that is secretly a demon gang threatens to steal the universe's sanity, " << endl;
+    cout << "the K-pop trio must put aside their differences and unite to protect humanity." << endl;
+    cout << "" << endl; 
+
+    // Show three inspirational quotes from the movie
+    cout << "Movie Quotes" << endl;\
+    cout << "" << endl; 
+    cout << "That's the funny thing about hope, no one gets to decide when you feel it." << endl;
+    cout << "- Rumi" << endl;
+    cout << "Your strength was never just in your songs. It’s in the heart that writes them." << endl;
+    cout << "- Celine" << endl;
+    cout << "You left them. Left them behind. Failed them. These are a constant reminder of my shame. A shame I can never escape." << endl;
+    cout << "- Jinu" << endl;
+    cout << "" << endl; 
+
+}
